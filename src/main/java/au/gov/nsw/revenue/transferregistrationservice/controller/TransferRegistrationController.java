e8bdb56b91e0abd4e13f383ea683de0c3f372e22
amolksagar <kksagar.amol@gmail.com>
2021-07-04 19:32:41 +1000

    profile based changes

--- src/main/java/au/gov/nsw/revenue/transferregistrationservice/controller/TransferRegistrationController.java
+++ src/main/java/au/gov/nsw/revenue/transferregistrationservice/controller/TransferRegistrationController.java
@@ -3,10 +3,10 @@ package au.gov.nsw.revenue.transferregistrationservice.controller;
 import au.gov.nsw.revenue.transferregistrationservice.openapi.api.TransferRegistrationApi;
 import au.gov.nsw.revenue.transferregistrationservice.openapi.model.Person;
 import au.gov.nsw.revenue.transferregistrationservice.openapi.model.SearchPersonRequest;
-import au.gov.nsw.revenue.transferregistrationservice.openapi.model.ServiceStatus;
 import au.gov.nsw.revenue.transferregistrationservice.openapi.model.Vehicle;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @Tag(name = "Transfer Registration API",description = "The API endpoints enables a client to link or unlink a Person to a Vehicle.")
 public class TransferRegistrationController implements TransferRegistrationApi {
+
     @Override
     public ResponseEntity<Person> createPerson(Person person) {
-        log.info("Test1234");
+        log.info("createPerson called ");
         return ResponseEntity.status(HttpStatus.CREATED).body(person);
     }
 
