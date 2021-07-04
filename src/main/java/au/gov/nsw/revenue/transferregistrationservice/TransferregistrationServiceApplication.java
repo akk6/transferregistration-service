f4ef396c5d14847247768e1770b18367c9a662fc
amolksagar <kksagar.amol@gmail.com>
2021-07-04 13:49:52 +1000

    changes for oas

--- src/main/java/au/gov/nsw/revenue/transferregistrationservice/TransferregistrationServiceApplication.java
+++ src/main/java/au/gov/nsw/revenue/transferregistrationservice/TransferregistrationServiceApplication.java
@@ -2,8 +2,10 @@ package au.gov.nsw.revenue.transferregistrationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"au.gov.nsw.revenue.transferregistrationservice"})
 public class TransferregistrationServiceApplication {
 
 	public static void main(String[] args) {
