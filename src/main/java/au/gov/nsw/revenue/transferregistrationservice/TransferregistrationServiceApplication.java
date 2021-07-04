3941a5d8c64ff7a09fb86e11321215b39c77ee32
amolksagar <kksagar.amol@gmail.com>
2021-07-04 11:06:06 +1000

    Initial Changes

---
+++ src/main/java/au/gov/nsw/revenue/transferregistrationservice/TransferregistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package au.gov.nsw.revenue.transferregistrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferregistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferregistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/gov/nsw/revenue/transferregistrationservice/TransferregistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package au.gov.nsw.revenue.transferregistrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferregistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
